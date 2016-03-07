a2cf6cf28540581c6ed6e0967900b5b9cf5e3796
David Lu <davidelu93@gmail.com>
2016-03-07 18:57:43 -0500

    rename deduplicateTag to tags

--- client/middlewares/tags.js
+++ client/middlewares/tags.js
@@ -6,10 +6,16 @@ const {type: ADD} = add()
 
 export default ({getState}) => (next) => (action) => {
   let {type, payload} = action
+
+  // deduplicate tags
+
   if (type !== MODIFY || payload.tagAction.type !== ADD) {
     return next(action)
   }
+
   let {index, tagAction: {payload: {tag}}} = payload
+
   let {ui: {bookmarks: {items}}} = getState()
+
   return items[index].tags.includes(tag) ? null : next(action)
 }
